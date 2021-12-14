6ac6fb71cdced8f191fd5cdfed4d8d360b48177a
Marium Binte Ibrahim Ema <32203563+Marium-Ibrahim@users.noreply.github.com>
2021-12-13 23:36:35 -0500

    Create 996A.c

---
+++ Codeforces/996A.c
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sz 5
+
+int main(){
+    
+    int n, count=0;
+    cin>>n;
+    
+    count+=n/100;
+    n=n%100;
+    count+=n/20;
+    n=n%20;
+    count+=n/10;
+    n=n%10;
+    count+=n/5;
+    n=n%5;
+    count+=n/1;
+
+    cout<<count<<endl;
+    
+    return 0;
+}
